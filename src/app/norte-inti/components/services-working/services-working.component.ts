826ebace5d66b0d973ee1b5254c283856b88e44c
Jeason Cueva Espinoza <jeasoncues@gmail.com>
2023-06-21 13:00:36 -0500

    fix: main component and modules

---
+++ src/app/norte-inti/components/services-working/services-working.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'services-working',
+  templateUrl: './services-working.component.html',
+  styleUrls: ['./services-working.component.css']
+})
+export class ServicesWorkingComponent {
+
+}
--- src/app/norte-inti/norte-inti.module.ts
+++ src/app/norte-inti/norte-inti.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './pages/home/home.component';
 import { BannerMainComponent } from './components/banner-main/banner-main.component';
+import { ServicesWorkingComponent } from './components/services-working/services-working.component';
 
 @NgModule({
-  declarations: [HomeComponent, BannerMainComponent],
+  declarations: [HomeComponent, BannerMainComponent, ServicesWorkingComponent],
   imports: [
     CommonModule
   ],
